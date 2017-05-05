e165afbda52c84c5c5fe147185d7cf367fac74d8
Minh T. Vu <minhtvu@mymail.mines.edu>
2017-05-04 19:56:20 -0600

    Fixed

--- src/GuiApplication.java
+++ src/GuiApplication.java
@@ -1,18 +1,18 @@
 /*
-   Copyright 2008 Christopher Painter-Wakefield
-
-   Licensed under the Apache License, Version 2.0 (the "License");
-   you may not use this file except in compliance with the License.
-   You may obtain a copy of the License at
-
-       http://www.apache.org/licenses/LICENSE-2.0
-
-   Unless required by applicable law or agreed to in writing, software
-   distributed under the License is distributed on an "AS IS" BASIS,
-   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-   See the License for the specific language governing permissions and
-   limitations under the License.
-*/
+ Copyright 2008 Christopher Painter-Wakefield
+ 
+ Licensed under the Apache License, Version 2.0 (the "License");
+ you may not use this file except in compliance with the License.
+ You may obtain a copy of the License at
+ 
+ http://www.apache.org/licenses/LICENSE-2.0
+ 
+ Unless required by applicable law or agreed to in writing, software
+ distributed under the License is distributed on an "AS IS" BASIS,
+ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ See the License for the specific language governing permissions and
+ limitations under the License.
+ */
 
 import java.awt.*;
 import java.awt.event.ActionEvent;
@@ -30,10 +30,10 @@ public class GuiApplication implements ActionListener, ListSelectionListener {
     // Model
     GuiModel model;
     //GuiModel[] modelArray;
-
+    
     // UI components
     JFrame 		frame;
-
+    
     JPanel		searchPane;
     JLabel      cityLabel;
     JComboBox	citySelection;
@@ -42,10 +42,10 @@ public class GuiApplication implements ActionListener, ListSelectionListener {
     JComboBox   searchType;
     JTextField  searchText;
     JButton		searchButton;
-
+    
     JPanel      resultsPane;
     JTable      searchResultsTable;
-
+    
     JPanel      buttonPane;
     JButton     addArtistButton;
     JButton     addAlbumButton;
@@ -54,32 +54,32 @@ public class GuiApplication implements ActionListener, ListSelectionListener {
     JButton     quitButton;
     
     final City startingCity = City.Chicago;
-
+    
     public static void main(String[] args) {
         GuiApplication app = new GuiApplication();
         app.open();
     }
-
+    
     void open() {
         model = new GuiModel();
         model.setColumns(ChicagoDataLabelArray);
         GuiLoginDialog loginDialog = new GuiLoginDialog(model);
         loginDialog.open();
-
+        
         frame = new JFrame("Crime Database Interface");
         frame.setResizable(false);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-
+        
         Container cp = frame.getContentPane();
-
+        
         searchPane = new JPanel();
         searchPane.setLayout(new FlowLayout());
         searchPane.setBorder(new EtchedBorder());
         cp.add(searchPane, BorderLayout.NORTH);
-
+        
         cityLabel = new JLabel("City:");
         searchPane.add(cityLabel);
-
+        
         citySelection = new JComboBox();
         citySelection.addItem("Chicago");
         citySelection.addItem("Denver");
@@ -89,26 +89,26 @@ public class GuiApplication implements ActionListener, ListSelectionListener {
         //The stuff for the search options that may vary between cities
         searchLabel = new JLabel("Search by:");
         searchPane.add(searchLabel);
-
+        
         searchType = new JComboBox();
-       // searchType.addItem("option 1");
+        // searchType.addItem("option 1");
         //searchType.addItem("option 2");
-       // searchType.addItem("option 3");
+        // searchType.addItem("option 3");
         updateSearchByOptions(startingCity);
         searchType.addActionListener(this);
         searchPane.add(searchType);
-
-        searchText = new JTextField(20);
+        
+        searchText = new JTextField(10);
         searchPane.add(searchText);
-
+        
         searchButton = new JButton("Search");
         searchButton.addActionListener(this);
         searchPane.add(searchButton);
-
+        
         resultsPane = new JPanel();
         resultsPane.setLayout(new FlowLayout());
         cp.add(resultsPane, BorderLayout.CENTER);
-
+        
         
         searchResultsTable = new JTable();
         searchResultsTable.setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
@@ -118,22 +118,22 @@ public class GuiApplication implements ActionListener, ListSelectionListener {
         JScrollPane scroller = new JScrollPane(searchResultsTable);
         scroller.setPreferredSize(new Dimension(600, 350));
         resultsPane.add(scroller);
-
+        
         buttonPane = new JPanel();
         buttonPane.setLayout(new FlowLayout());
         cp.add(buttonPane, BorderLayout.SOUTH);
-
+        
         quitButton = new JButton("Quit");
         quitButton.addActionListener(this);
-
+        
         buttonPane.add(quitButton);
-
+        
         frame.pack();
         frame.setVisible(true);
-
+        
     }
-
-     // process action events
+    
+    // process action events
     public void actionPerformed(ActionEvent e) {
         if (e.getSource() == this.searchButton) {
             search();
@@ -154,92 +154,92 @@ public class GuiApplication implements ActionListener, ListSelectionListener {
             System.exit(0);
         }
         else if(e.getSource() == this.citySelection){
-        	onCityTypeChanged();
+            onCityTypeChanged();
         }
     }
     private void onCityTypeChanged(){
-    	City c = City.valueOf((String)citySelection.getSelectedItem());
-    	updateSearchByOptions(c);
-    	//Will also need to change the column labels in the list of rows
-    	updateDataLabels(c);
+        City c = City.valueOf((String)citySelection.getSelectedItem());
+        updateSearchByOptions(c);
+        //Will also need to change the column labels in the list of rows
+        updateDataLabels(c);
     }
-    static Object[] LADataLabelArray = {"Date", "Description", "Address", "Zip"};
-    static Object[] ChicagoDataLabelArray = {"Date", "Description", "Block"};
-    static Object[] DenverDataLabelArray = {"Date", "Description", "Address", "Neighborhood"};
+    static Object[] LADataLabelArray = {"CrimeDate", "Description", "Address", "Zip"};
+    static Object[] ChicagoDataLabelArray = {"CrimeDate", "Description", "CityBlock"};
+    static Object[] DenverDataLabelArray = {"CrimeDate", "Description", "Address", "Neighborhood"};
     static public int currentCityColumnCount(City city){
-    	switch(city){
-		case Chicago:
-			return ChicagoDataLabelArray.length;
-		case Denver:
-			return DenverDataLabelArray.length;
-		case LA:
-			return LADataLabelArray.length;
-		default:
-			break;
-    	}
-    	System.out.println("failed to match city in currentCityColumnCount(City)");
-    	return -1;
+        switch(city){
+            case Chicago:
+                return ChicagoDataLabelArray.length;
+            case Denver:
+                return DenverDataLabelArray.length;
+            case LA:
+                return LADataLabelArray.length;
+            default:
+                break;
+        }
+        System.out.println("failed to match city in currentCityColumnCount(City)");
+        return -1;
     }
     private void updateDataLabels(City c){
-    	switch(c){
-		case Chicago:
-			ChicagoDataLabels();
-			break;
-		case Denver:
-			DenverDataLabels();
-			break;
-		case LA:
-			LADataLabels();
-			break;
-		default:
-			break;
-    	}
+        switch(c){
+            case Chicago:
+                ChicagoDataLabels();
+                break;
+            case Denver:
+                DenverDataLabels();
+                break;
+            case LA:
+                LADataLabels();
+                break;
+            default:
+                break;
+        }
     }
     private void LADataLabels(){
-    	model.setColumns(LADataLabelArray);
+        model.setColumns(LADataLabelArray);
     }
     private void ChicagoDataLabels(){
-    	model.setColumns(ChicagoDataLabelArray);
+        model.setColumns(ChicagoDataLabelArray);
     }
     private void DenverDataLabels(){
-    	model.setColumns(DenverDataLabelArray);
+        model.setColumns(DenverDataLabelArray);
     }
     private void updateSearchByOptions(City c){
-    	searchType.removeAllItems();
-    	switch(c){
-		case Chicago:
-			ChicagoSearchByOptions();
-			break;
-		case Denver:
-			DenverSearchByOptions();
-			break;
-		case LA:
-			LASearchByOptions();
-			break;
-		default:
-			System.out.println("This should be impossible - updateSearchByOptions");
-			searchType.addItem("ERROR");
-			break;
-    	}
+        searchType.removeAllItems();
+        switch(c){
+            case Chicago:
+                ChicagoSearchByOptions();
+                break;
+            case Denver:
+                DenverSearchByOptions();
+                break;
+            case LA:
+                LASearchByOptions();
+                break;
+            default:
+                System.out.println("This should be impossible - updateSearchByOptions");
+                searchType.addItem("ERROR");
+                break;
+        }
     }
     private void LASearchByOptions(){
-    	for(int i = 0; i < LADataLabelArray.length; i++){
-    		searchType.addItem(LADataLabelArray[i]);
-    	}
+        for(int i = 0; i < LADataLabelArray.length; i++){
+            searchType.addItem(LADataLabelArray[i]);
+        }
     }
     private void ChicagoSearchByOptions(){
-    	for(int i = 0; i < ChicagoDataLabelArray.length; i++){
-    		searchType.addItem(ChicagoDataLabelArray[i]);
-    	}
+        for(int i = 0; i < ChicagoDataLabelArray.length; i++){
+            searchType.addItem(ChicagoDataLabelArray[i]);
+        }
     }
     private void DenverSearchByOptions(){
-    	for(int i = 0; i < DenverDataLabelArray.length; i++){
-    		searchType.addItem(DenverDataLabelArray[i]);
-    	}
+        for(int i = 0; i < DenverDataLabelArray.length; i++){
+            searchType.addItem(DenverDataLabelArray[i]);
+        }
     }
-
+    
     private void search() {
-    	City c = City.valueOf((String)citySelection.getSelectedItem());
+        City c = City.valueOf((String)citySelection.getSelectedItem());
         try {
             model.search(c, (String)searchType.getSelectedItem(), searchText.getText());
         } catch (SQLException ex) {
@@ -258,12 +258,12 @@ public class GuiApplication implements ActionListener, ListSelectionListener {
         }
         model.setSelectedRow(searchResultsTable.getSelectedRow());
     }
-
+    
     private void sqlExceptionHandler(SQLException e) {
         JOptionPane.showMessageDialog(frame,
-                "Database error: " + e.getMessage(),
-                "Database error",
-                ERROR_MESSAGE);
+                                      "Database error: " + e.getMessage(),
+                                      "Database error",
+                                      ERROR_MESSAGE);
     }
-
+    
 }
--- src/GuiModel.java
+++ src/GuiModel.java
@@ -3,18 +3,18 @@ import java.sql.*;
 import java.util.Vector;
 
 public class GuiModel extends DefaultTableModel {
-
+    
     Connection db;
     Vector<Integer> albumIDs;   // will hold album ID values corresponding to search results
     int selectedRow;            // current selected row
-
+    
     public GuiModel() {
         Object[] a = {"Artist", "Album Title", "Year"};
         
         setColumnIdentifiers(a);
     }
     public void setColumns(Object[] newIDs){
-    	setColumnIdentifiers(newIDs);
+        setColumnIdentifiers(newIDs);
     }
     public void setSelectedRow(int selectedRow) {
         this.selectedRow = selectedRow;
@@ -22,8 +22,8 @@ public class GuiModel extends DefaultTableModel {
     public void removeSelectedRow() {
         removeRow(selectedRow);
     }
-
-
+    
+    
     public int getAlbumID() {
         if (selectedRow != -1) {
             return albumIDs.elementAt(selectedRow);
@@ -32,7 +32,7 @@ public class GuiModel extends DefaultTableModel {
             return -1;
         }
     }
-
+    
     public String getArtist() {
         if (selectedRow != -1) {
             return (String) getValueAt(selectedRow, 0);
@@ -41,7 +41,7 @@ public class GuiModel extends DefaultTableModel {
             return "";
         }
     }
-
+    
     public String getAlbumTitle() {
         if (selectedRow != -1) {
             return (String) getValueAt(selectedRow, 1);
@@ -50,7 +50,7 @@ public class GuiModel extends DefaultTableModel {
             return "";
         }
     }
-
+    
     public String getAlbumYear() {
         if (selectedRow != -1) {
             return (String) getValueAt(selectedRow, 2);
@@ -59,39 +59,39 @@ public class GuiModel extends DefaultTableModel {
             return "";
         }
     }
-
+    
     public void login(String username, String password) throws SQLException, ClassNotFoundException {
-
+        
         Class.forName("org.postgresql.Driver");
         String connectString = "jdbc:postgresql://flowers.mines.edu/csci403";
-
+        
         db = DriverManager.getConnection(connectString, username, password);
     }
-
+    
     public void search(City city, String by, String val) throws SQLException {
         ResultSet rs;
         switch(city){
-		case Chicago:
-			rs = searchChicago(by, val);
-			break;
-		case Denver:
-			rs = searchDenver(by, val);
-			break;
-		case LA:
-			rs = searchLA(by, val);
-			break;
-		default:
-            throw new RuntimeException("Invalid search request");
+            case Chicago:
+                rs = searchChicago(by, val);
+                break;
+            case Denver:
+                rs = searchDenver(by, val);
+                break;
+            case LA:
+                rs = searchLA(by, val);
+                break;
+            default:
+                throw new RuntimeException("Invalid search request");
         }
-//        if (by.equals("Artist")) {
-//            rs = searchByArtist(val);
-//        }
-//        else if (by.equals("Album")) {
-//            rs = searchByAlbum(val);
-//        }
-//        else {
-//            throw new RuntimeException("Invalid search request");
-//        }
+        //        if (by.equals("Artist")) {
+        //            rs = searchByArtist(val);
+        //        }
+        //        else if (by.equals("Album")) {
+        //            rs = searchByAlbum(val);
+        //        }
+        //        else {
+        //            throw new RuntimeException("Invalid search request");
+        //        }
         if (rs == null) {
             setNumRows(1);
             setValueAt("Search by " + by + ":", 0, 0);
@@ -102,14 +102,14 @@ public class GuiModel extends DefaultTableModel {
             dataVector = new Vector<Vector<Object>>();
             //albumIDs = new Vector<>();
             while (rs.next()) {
-                Vector<Object> row = new Vector<>();
+                Vector<Object> row = new Vector<Object>();
                 int colCount = GuiApplication.currentCityColumnCount(city);
                 for (int i = 0; i < colCount; i++) {
-					row.add(rs.getString(i + 1));
-				}
-//                row.add(rs.getString(1));
-//                row.add(rs.getString(2));
-//                row.add(rs.getString(3));
+                    row.add(rs.getString(i + 1));
+                }
+                //                row.add(rs.getString(1));
+                //                row.add(rs.getString(2));
+                //                row.add(rs.getString(3));
                 dataVector.add(row);
                 //albumIDs.add(rs.getInt(4));
             }
@@ -117,37 +117,37 @@ public class GuiModel extends DefaultTableModel {
         }
     }
     private ResultSet searchLA(String by, String val) throws SQLException{
-    	String query =
-    			"SELECT date, description, address, zip " +
-    					"FROM LACrimeData " +
-    					"WHERE lower(" + by + ") LIKE lower(?) " +
-    					"ORDER BY date, description, address";
-
-    	PreparedStatement ps = db.prepareStatement(query);
-    	ps.setString(1, "%" + val + "%");
-    	return ps.executeQuery();
+        String query =
+        "SELECT crimedate, description, address, zip " +
+        "FROM LACrimeData " +
+        "WHERE lower(" + by + ") LIKE lower(?) " +
+        "ORDER BY crimedate, description, address";
+        
+        PreparedStatement ps = db.prepareStatement(query);
+        ps.setString(1, "%" + val + "%");
+        return ps.executeQuery();
     }
     private ResultSet searchDenver(String by, String val) throws SQLException{
-    	String query =
-    			"SELECT date, description, address, neighborhood " +
-    					"FROM denverCrimeData " +
-    					"WHERE lower(" + by + ") LIKE lower(?) " +
-    					"ORDER BY date, description, address";
-
-    	PreparedStatement ps = db.prepareStatement(query);
-    	ps.setString(1, "%" + val + "%");
-    	return ps.executeQuery();
+        String query =
+        "SELECT crimedate, description, address, neighborhood " +
+        "FROM denverCrimeData " +
+        "WHERE lower(" + by + ") LIKE lower(?) " +
+        "ORDER BY crimedate, description, address";
+        
+        PreparedStatement ps = db.prepareStatement(query);
+        ps.setString(1, "%" + val + "%");
+        return ps.executeQuery();
     }
     private ResultSet searchChicago(String by, String val) throws SQLException{
-    	String query =
-    			"SELECT date, description, block " +
-    					"FROM chicagoCrimeData " +
-    					"WHERE lower(" + by + ") LIKE lower(?) " +
-    					"ORDER BY date, description";
-
-    	PreparedStatement ps = db.prepareStatement(query);
-    	ps.setString(1, "%" + val + "%");
-    	return ps.executeQuery();
+        String query =
+        "SELECT crimedate, description, cityblock " +
+        "FROM chicagoCrimeData " +
+        "WHERE lower(" + by + ") LIKE lower(?) " +
+        "ORDER BY crimedate, description";
+        
+        PreparedStatement ps = db.prepareStatement(query);
+        ps.setString(1, "%" + val + "%");
+        return ps.executeQuery();
     }
-
+    
 }
